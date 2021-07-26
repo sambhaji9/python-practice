e69c83939e41a1dc375e48774a481572d9c5503b
Sambhaji Mane <mane.sambhaji9@gmail.com>
2021-07-26 21:56:13 +0530

    Add some practice files for basic operations

---
+++ numbers.py
@@ -0,0 +1,11 @@
+myint = 7
+print("integer ", myint)
+
+myfloat = 7.0
+print("float", myfloat)
+
+myfloat = float(7)
+print("float", myfloat)
+
+a, b = 3, 4
+print(a, b)
---
+++ operators.py
@@ -0,0 +1,5 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11%3
+print(remainder)
