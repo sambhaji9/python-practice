25c4d8e7a07e4e583583ca676ae95c29ddb4df7d
Sambhaji Mane <mane.sambhaji9@gmail.com>
2021-11-14 12:32:12 +0530

    Added example for result calculation

--- for.py
+++ for.py
@@ -2,3 +2,17 @@
 words = ['cat', 'window', 'defenestrate']
 for w in words:
     print(w, len(w))
+
+genre = ['pop', 'rock', 'jazz']
+
+
+# iterate over the list using index
+for i in range(len(genre)):
+    print("i like", genre[i])
+
+
+digits = [0, 1, 5]
+for i in digits:
+    print(i)
+else:
+    print("no items left")
