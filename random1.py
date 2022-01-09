77b2535cf97397d66519bd0c718d064b590d07d9
Sambhaji Mane <mane.sambhaji9@gmail.com>
2022-01-09 19:53:02 +0530

    Added examples for random, statistics, maths

---
+++ random1.py
@@ -0,0 +1,7 @@
+import random
+
+print(random.choice(['apple', 'pear', 'banana']))
+
+print(random.random())
+
+print(random.randrange(6))
---
+++ statistics1.py
@@ -0,0 +1,6 @@
+import statistics
+data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
+
+print('mean ', statistics.mean(data))
+print('median', statistics.median(data))
+print('variance', statistics.variance(data))
